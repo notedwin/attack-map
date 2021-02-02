9bcc78d9bd925f4521ece231d9a3722b03246526
Edwin Zamudio <ezamudio@hawk.iit.edu>
2021-02-02 17:58:04 -0600

    random data parsed by map

--- ip2lat.js
+++
@@ -1,4 +0,0 @@
-var fs = require('fs');
-
-let result = fs.readFileSync("ssh.out",'utf8')
-console.log(result)
--- sock.py
+++ sock.py
@@ -6,15 +6,10 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-#ipdata = ipdata.IPData(os.environ.get("IPDATA_KEY"))
-#a = ipdata.lookup(line.strip())
-#arr.append([line.strip(),a["latitude"],a["longitude"]])
-
+ipdata = ipdata.IPData('8d90c6670a1a382d168336e0414e6c1aa7b4ed374579437293d07f63')
 
 HOST = os.environ.get("HOST") # Standard loopback interface address (localhost)
-PORT = int(os.environ.get("PORT"))       # Port to listen on (non-privileged ports are > 1023)
-print(type(HOST),type(PORT))
-x = []
+PORT = int(os.environ.get("PORT")) # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -25,13 +20,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         while True:
             data = conn.recv(1024).decode()
             try:
-                y = json.dumps(data)
-                y = json.loads(y)
-                print(y["message"])
-                print(y)
+                data_arr = data.split()
+                if(data_arr[3] == "root" or data_arr[3] == "pi"):
+                    user = data_arr[3]
+                    ip = data_arr[5]
+                else:
+                    user = data_arr[5]
+                    ip = data_arr[7]
+                port = data_arr[-2]
+                data = ipdata.lookup(ip)
+                lat_long = [data["latitude"],data["longitude"]]
+                print(user,ip,port,lat_long)
             except:
                 print("fail")
-                print(data)
+                # skip
             if not data:
                 break
-print(x)
