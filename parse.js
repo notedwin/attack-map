107393e83bb6167be459b54d204dc474226b7cbe
Edwin Zamudio <ezamudio@hawk.iit.edu>
2021-05-15 03:40:21 -0500

    cleaned up code

--- parse.js
+++ parse.js
@@ -18,168 +18,155 @@ redis.on("connect", () => {
 
 app.set("view engine", "ejs");
 
-const port = process.env.LOG_PORT;
-const html_port = process.env.SERVER_PORT;
-
-// the line below will go down as the worlds best database LOL xD
-
 app.use(express.static(__dirname + "/public"));
 //app.set('views', path.join(__dirname, '/views'));
 
 app.set("views", path.join(__dirname, "./views"));
+
 // render based on 50 most recent access
+//make it render real-time?
+async function getData() {
+  try {
+    lat_long = new Array();
+    min_ago = String(Math.floor((Date.now() - 1440 * 60000) / 1000));
+    hackers = await redis.zrangebyscoreAsync("hackers", min_ago, "inf");
+    for (var index = 0; index < hackers.length; ++index) {
+      hacker = await redis.hgetallAsync(hackers[index]);
+      var date = new Date(hackers[index] * 1000);
+      var hours = date.getHours();
+      var minutes = "0" + date.getMinutes();
+      var seconds = "0" + date.getSeconds();
+      var formattedTime = hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
+      hacker.time = formattedTime;
+      lat_long.push(hacker);
+    }
+    return lat_long
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 app.get("/globe", async function (req, res) {
-  lat_long = new Array();
-  try {
-    min_ago = String(Math.floor((Date.now() - 1440 * 60000) / 1000));
-    hackers = await redis.zrangebyscoreAsync("hackers", min_ago, "inf");
-    for (var index = 0; index < hackers.length; ++index) {
-      hacker = await redis.hgetallAsync(hackers[index]);
-      var date = new Date(hackers[index] * 1000);
-      var hours = date.getHours();
-      var minutes = "0" + date.getMinutes();
-      var seconds = "0" + date.getSeconds();
-      var formattedTime = hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
-      hacker.time = formattedTime;
-      lat_long.push(hacker);
-    }
-    console.log(hackers);
-  } catch (err) {
-    console.log(err);
-  }
+  lat_long = await getData();
+  console.log(lat_long);
   res.render("globe", { loc: lat_long });
 });
-
-app.get("/nginx", async function (req, res) {
-  lat_long = new Array();
-  try {
-  } catch (err) {
-    console.log(err);
-  }
-  res.render("globe", { loc: lat_long });
-});
-
-
 
 app.get("", async function (req, res) {
-  lat_long = new Array();
-  try {
-    min_ago = String(Math.floor((Date.now() - 1440 * 60000) / 1000));
-    hackers = await redis.zrangebyscoreAsync("hackers", min_ago, "inf");
-    for (var index = 0; index < hackers.length; ++index) {
-      hacker = await redis.hgetallAsync(hackers[index]);
-      var date = new Date(hackers[index] * 1000);
-      var hours = date.getHours();
-      var minutes = "0" + date.getMinutes();
-      var seconds = "0" + date.getSeconds();
-      var formattedTime = hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
-      hacker.time = formattedTime;
-      lat_long.push(hacker);
-    }
-    console.log(hackers);
-  } catch (err) {
-    console.log(err);
-  }
+  lat_long = await getData();
+  console.log(lat_long);
   res.render("index", { loc: lat_long });
 });
 
-app.listen(html_port, () => {
-  console.log(`Example app listening at http://localhost:${html_port}`);
-});
-
-const API_URL = "http://ip-api.com/json/";
-var server = net.createServer();
-
-async function data2ip(data) {
-  data_arr = data.toString().split(/[ ,]+/);
-  if (data_arr[4] === "root" || data_arr[4] === "pi") {
-    var user = data_arr[4];
-    var ip = data_arr[6];
-    var port = data_arr.reverse()[1];
-  } else {
-    var user = data_arr[6];
-    var ip = data_arr[8];
-    var port = data_arr.reverse()[1];
-  }
-
-  // can keep this since we have the redis behind
-  const ipLocation = await doApiCall(ip);
-  const { lon, lat } = ipLocation;
-
-  time = String(Math.floor(Date.now() / 1000));
-
-  redis.HMSET(
-    time,
-    "user",
-    user,
-    "ip",
-    ip,
-    "port",
-    port,
-    "lon",
-    lon,
-    "lat",
-    lat
-  );
-  redis.zadd("hackers", time, time);
-}
-
-async function retrieveLocationFromAPI(ip) {
-  const { data, status, statusText } = await Axios.get(`${API_URL}/${ip}`);
-  if (!data || status !== 200 || data.status !== "success") {
-    throw new Error(`Unsuccessful request (${status}): ${data}`);
-  }
-  // Sometimes the API returns empty values
-  // defaulting every empty string to 'none'
-  for (const key in data) {
-    if (data[key] === "") {
-      data[key] = "none";
-    }
-  }
-  return data;
-}
-
-async function doApiCall(ip) {
+async function populateRedis(ip, user, port, server) {
   // I don't know if this is the correct way to do async but it works
   // Memoization, prevent API call for the same IP
-  exists = await redis.existsAsync(ip);
-  if (exists == 1) {
-    console.log("redis has the IP");
-    data = await redis.hgetallAsync(ip);
-    return { lon: data.lon, lat: data.lat };
-  } else {
-    const data = await retrieveLocationFromAPI(ip);
-    console.log(data);
+  try{
+    var data = {};
+    exists = await redis.existsAsync(ip);
+    if (exists == 1) {
+      data = await redis.hgetallAsync(ip);
+    } else {
+      const API_URL = "http://ip-api.com/json/";
+      const { data, status, statusText } = await Axios.get(`${API_URL}/${ip}`);
+      if (!data || status !== 200 || data.status !== "success") {
+        throw new Error(`Unsuccessful request (${status}): ${data}`);
+      }
+    }
+
+    console.log(data,server);
+    time = String(Math.floor(Date.now() / 1000));
+
     redis.HMSET(ip, "lon", data.lon, "lat", data.lat);
-    //return only data we need
-    return { lon: data.lon, lat: data.lat };
+    redis.HMSET(time,"user",user,"ip",ip,"port",port,"lon",data.lon,"lat",data.lat,"dest",server);
+    redis.zadd("hackers", time, time);
+  }catch (err) {
+    console.log(err);
   }
 }
 
-server.on("close", function () {
-  console.log("Server closed !");
-});
+//server is rsyslog server
+//server_c is cowrie log server
+
+var server = net.createServer();
+var server_c = net.createServer();
+var server_n = net.createServer();
 
 server.on("connection", function (socket) {
   console.log("connect to log server.");
 
   socket.on("data", async function (data) {
-    await data2ip(data);
+    data_arr = data.toString().split(/[ ,]+/);
+    //insecure put this in .env
+    if (data_arr[4] === "root" || data_arr[4] === "pi") {
+      var user = data_arr[4];
+      var ip = data_arr[6];
+      var port = data_arr.reverse()[1];
+    } else {
+      var user = data_arr[6];
+      var ip = data_arr[8];
+      var port = data_arr.reverse()[1];
+    }
+    await populateRedis(ip, user, port,1);
   });
+});
 
+server_c.on("connection", function (socket) {
+  console.log("connect to log server.");
+
+  socket.on("data", async function (data) {
+    p = data
+      .toString()
+      .replace(/[ =|,]/g, ",")
+      .split(/[ ,]+/)
+      .reverse();
+    var user = p[2];
+    var ip = p[6];
+    var port = Math.floor(Math.random() * (6000 - 1024 + 1)) + 1024;
+    console.log(user,ip,port)
+    await populateRedis(ip,user,port,2);
+  });
 
   socket.on("close", function (error) {
     console.log("Socket closed!");
-    if (error) {
-      console.log("Socket was closed coz of transmission error");
-    }
+  });
+});
+
+server_n.on("connection", function (socket) {
+  console.log("connect to nginx server.");
+
+  socket.on("data", async function (data) {
+    data_arr = JSON.parse(data.toString().substring(40));
+    var port = Math.floor(Math.random() * (6000 - 1024 + 1)) + 1024;;
+    var user = "chrome";
+    var ip = data_arr.remote_addr;
+    console.log(port,user,ip)
+    await populateRedis(ip,user,port,1);
   });
 
+  socket.on("close", function (error) {
+    console.log("Socket closed!");
+  });
 });
 
-server.on("listening", function () {
-  console.log("Log server is listening");
+
+//nginx logs
+server_n.listen(process.env.NGINX, () => {
+  console.log("Listening for NGINX access logs");
 });
 
-server.listen(port, '127.0.0.1');
+//rsyslog-ssh
+server.listen(process.env.RSYSLOG, "127.0.0.1", () => {
+  console.log("Listening for RSYSLOG-SSH");
+});
+//cowrie
+server_c.listen(process.env.COWRIE, () => {
+  console.log("Listening for Cowrie Logs from AWS ");
+});
+
+//front-end 3000
+app.listen(process.env.FRONT_END, () => {
+  console.log(
+    `Example app listening at http://localhost:${process.env.FRONT_END}`
+  );
+});
