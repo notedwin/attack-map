f38fafbd0885756ddc676e13d91fe54a2445dfe1
Edwin Zamudio <ezamudio@hawk.iit.edu>
2022-08-02 22:46:15 -0500

    unique ip addresses

--- src/bin/ports.rs
+++ src/bin/ports.rs
@@ -4,9 +4,17 @@ use rustypi::{pull_hackers, Hacker};
 
 fn main() {
     let hackers: Vec<Hacker> = pull_hackers();
-
+    // array of unique IPs
+    let mut unique_ips: Vec<String> = Vec::new();
     for hacker in hackers {
-        let ip:SocketAddr = format!("{}:443", hacker.ip.clone()).parse().expect("Could not parse ip");
+        if !unique_ips.contains(&hacker.ip) {
+            unique_ips.push(hacker.ip.to_string());
+        }
+    }
+
+    for raw_ip in unique_ips {
+
+        let ip:SocketAddr = format!("{}:443", raw_ip.clone()).parse().expect("Could not parse ip");
 
         // try to connect to the ip
         match TcpStream::connect_timeout(&ip, std::time::Duration::from_secs(1)) {
@@ -17,5 +25,14 @@ fn main() {
                 continue;
             }
         }
+        //let mut file = std::fs::File::create("ips.txt").expect("Could not create file");
+        //file.write_all(format!("{}\n", ip).as_bytes()).expect("Could not write to file");
+        // do not search ips that we have previously found
+        //if !ips.contains(&ip) {
+        //    ips.push(ip);
+        //}
+        //println!("{}", ip);
+
+        // report
     }
 }
